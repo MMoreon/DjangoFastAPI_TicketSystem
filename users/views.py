b5287ec3140975ca457484e66135caf427401259
MMoreon <mmoreon@outlook.com>
2026-09-18 15:20:49 +0300

    add index and urls

--- users/views.py
+++ users/views.py
@@ -1,3 +1,11 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+def auth(req):
+    return render(req, 'users/index.html')
+
+def home(req):
+    return HttpResponse("Успешно пройдено")
+
+def register(req):
+    return HttpResponse("Регистрация")
