2669d28ba9a17e96e00becafa3cb1c762a425fee
MMoreon <mmoreon@outlook.com>
2026-09-19 13:35:40 +0300

    add admin panel

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import User, Ticket, Comment, Screenshot
 
-# Register your models here.
+admin.site.register(User)
+admin.site.register(Ticket)
+admin.site.register(Comment)
+admin.site.register(Screenshot)
--- core/tests.py
+++ core/tests.py
@@ -1,8 +1,1 @@
 from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.auth, name='auth'),
-    path('home/', views.home),
-    path('register/', views.register, name='register'),
-]
